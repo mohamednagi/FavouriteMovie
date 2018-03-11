10c6a54988a747b8ac633429aa740372d4e032af
mohamed nagi <mohamed_nagi_93@hotmail.com>
2018-03-11 10:02:13 -0700

    Initial Commit

---
+++ MyFavMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFavMovies
+//
+//  Created by Sierra on 3/11/18.
+//  Copyright © 2018 Nagiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
